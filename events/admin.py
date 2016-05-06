3a8de56defb0753aef886cc90b63776aa6ea058c
Ranil Wijeyratne <me@ranil.ch>
2016-05-07 00:37:45 +0200

    improved gitignore

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Player, Event
+
+
+admin.site.register(Player)
+admin.site.register(Event)
--- events/views.py
+++ events/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the events index.")
+
+
+def detail(request, event_id):
+    return HttpResponse("You're looking at event %s." % event_id)
